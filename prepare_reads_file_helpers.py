f723acad939c6c18c99249e2857efaf811e65e33
martynaut <martyna.urbanek@gmail.com>
2019-06-05 07:43:35 +0200

    algorithm change

--- prepare_reads_file_helpers.py
+++ prepare_reads_file_helpers.py
@@ -1,7 +1,6 @@
 from lxml import html
 import requests
 import numpy as np
-import pandas as pd
 
 
 def parse_html(row):
@@ -15,17 +14,3 @@ def parse_html(row):
     except IndexError:
         return np.nan
 
-
-def prepare_hsa_files(hsa_gff_mirbase_file, output_folder):
-    names = ['chr', '.', 'type', 'start', 'stop', '.2', '-/+', '.3', 'desc']
-    data_chr = pd.read_csv(hsa_gff_mirbase_file, sep="\t", skiprows=13, names=names)
-    data_chr['ID'], \
-        data_chr['Alias'], \
-        data_chr['Name'] = zip(*data_chr['desc'].apply(lambda x: x.split(';')[:3]))
-    data_chr['From'] = data_chr['desc'].apply(lambda x: x.split(';')[3] if len(x.split(';')) >= 4 else None)
-    data_chr['From'] = data_chr['From'].str.extract(r'Derives_from\=(.+)', expand=False)
-    data_chr['ID'] = data_chr['ID'].str.extract(r'ID\=(.+)', expand=False)
-    data_chr['Alias'] = data_chr['Alias'].str.extract(r'Alias\=(.+)', expand=False)
-    data_chr['Name'] = data_chr['Name'].str.extract(r'Name\=(.+)', expand=False)
-    data_chr.drop('desc', inplace=True, axis=1)
-    data_chr.to_csv(output_folder + '/temp_reference/hsa_mirbase_coordinates.csv', index=False)
